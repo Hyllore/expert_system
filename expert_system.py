fd2e38ccc3f1b8613accccaa486a5a34c4f58590
Dorian ROLY <droly@e1r6p9.42.fr>
2017-06-20 17:22:00 +0200

    init 2

--- expert_system.py
+++ expert_system.py
@@ -3,10 +3,24 @@ import sys
 def filsdeputs(o):
     print o
 
+def checkfile(rules, facts, queries):
+    if (facts[0] != '=' and queries[0] != '?'):
+        print "error\n"
+
 if __name__ == "__main__":
     with open(sys.argv[1]) as f:
-            content = f.readlines()
-    content = [x.strip() for x in content]
-    filsdeputs(content)
-
-
+        content = f.readlines()
+    rules = []
+    for s in content:
+        s = s.strip()
+        if len(s) > 0:
+            rules.append(s)
+    rules = [x.strip() for x in rules]
+    queries = rules[-1]
+    facts = rules[-2]
+    del rules[1]
+    del rules[2]
+    filsdeputs(rules)
+    filsdeputs(facts)
+    filsdeputs(queries)
+    checkfile(rules, facts, queries)
