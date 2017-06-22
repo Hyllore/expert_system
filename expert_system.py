b082a38c39e610354a79ecf303b574144b7f7d08
Dorian ROLY <droly@e1r6p9.42.fr>
2017-06-22 17:20:07 +0200

    init 2

--- expert_system.py
+++ expert_system.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 import sys
 
 def filsdeputs(o):
@@ -8,14 +10,15 @@ def checkfile(rules, facts, queries):
         print "error\n"
         sys.exit()
     for r in rules:
-        tab = r.replace("  ", " ")
-        tab = tab.split(" ")
-        print tab
+        while (r.find("  ") != -1):
+            r = r.replace("  ", " ")
+        tab = r.split(" ")
+        print tab, "hey"
         i = 0
         while (i < len(tab) and tab[i] != "=>"):
             print "h"
             if (i < len(tab) and tab[i][0] == '!'):
-                if (tab[i][1].isalpha() == False or tab[i][1].isupper() == False):
+                if (len(tab[i]) != 2 or tab[i][1].isalpha() == False or tab[i][1].isupper() == False):
                     print "error 1\n"
                     sys.exit()
             elif (i < len(tab) and tab[i].isalpha() == False or tab[i].isupper() == False):
@@ -35,6 +38,39 @@ def checkfile(rules, facts, queries):
         if i >= len(tab):
             print "error 4\n"
             sys.exit()
+        elif (tab[i] == "=>"):
+            i += 1
+            if (i < len(tab)):
+                if (tab[i][0] == '!'):
+                    if (len(tab[i]) != 2 or tab[i][1].isalpha() == False or tab[i][1].isupper() == False):
+                        print "error 5\n"
+                        sys.exit()
+                elif (tab[i].isalpha() == False or tab[i].isupper() == False):
+                    print "error 6\n"
+                    sys.exit()
+                i += 1
+            else:
+                print "error 6\n"
+                sys.exit()
+            if (i < len(tab)):
+                print tab[i], "heho"
+                print str(i + 1), "and", str(len(tab))
+                if (tab[i] != "+" or i + 1 >= len(tab)):
+                    print "error 7\n"
+                    sys.exit()
+                i += 1
+                if (i < len(tab)):
+                    if (tab[i][0] == '!'):
+                        if (tab[i][1].isalpha() == False or tab[i][1].isupper() == False):
+                            print "error 8\n"
+                            sys.exit()
+                    elif (tab[i].isalpha() == False or tab[i].isupper() == False):
+                        print "error 9\n"
+                        sys.exit()
+                    i += 1
+                    if (i < len(tab)):
+                        print "error 10\n"
+                        sys.exit()
         print tab
 
 if __name__ == "__main__":
